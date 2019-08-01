5948a5b83af761bab9edd3adc904f4a49b07b6c4
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2019-08-01 17:32:40 -0300

    Getters e Setters adicionados

--- src/entities/Conta.java
+++ src/entities/Conta.java
@@ -16,6 +16,30 @@ public class Conta {
 		this.nomecliente = nomecliente;
 		this.balanco = balanco;
 	}
+
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+
+	public void setNumeroConta(int numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+
+	public String getNomecliente() {
+		return nomecliente;
+	}
+
+	public void setNomecliente(String nomecliente) {
+		this.nomecliente = nomecliente;
+	}
+
+	public double getBalanco() {
+		return balanco;
+	}
+
+	public void setBalanco(double balanco) {
+		this.balanco = balanco;
+	}
 	
 	
 	
